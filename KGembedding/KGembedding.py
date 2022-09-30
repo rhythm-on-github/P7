e7f4b7a2d5724b8a61d0ef1e11fb7705ca5d86f3
e <amha17@student.aau.dk>
2022-09-30 13:23:18 +0200

    do embedding stuff + update gitignore

--- KGembedding/KGembedding.py
+++ KGembedding/KGembedding.py
@@ -1,1 +1,15 @@
+from pykeen.triples import TriplesFactory
+from pykeen.pipeline import pipeline
+from pykeen.datasets.nations import NATIONS_TRAIN_PATH
+tf = TriplesFactory.from_path(NATIONS_TRAIN_PATH)
+training, testing = tf.split()
+result = pipeline(
+    training=training,
+    testing=testing,
+    model='TransE',
+    epochs=5,  # short epochs for testing - you should go higher
+)
+result.save_to_directory('doctests/test_unstratified_transe')
+
+
 
