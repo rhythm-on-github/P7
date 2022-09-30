0913b09d57c7ce8a2e95f0b934a33726fa9ed580
e <amha17@student.aau.dk>
2022-09-30 14:07:47 +0200

    add more arguments to pipeline

--- KGembedding/KGembedding.py
+++ KGembedding/KGembedding.py
@@ -10,12 +10,16 @@ FB15Kdir = os.path.join(dataDir, 'FB15K-237')
 
 training = os.path.join(FB15Kdir, 'train.txt')
 testing = os.path.join(FB15Kdir, 'test.txt')
+validation = os.path.join(FB15Kdir, 'valid.txt')
 
 result = pipeline(
     training=training,
     testing=testing,
+    validation=validation,
     model='TransE',
+    dimensions=100,
     epochs=5,  # short epochs for testing - you should go higher
+    device='gpu'
 )
 
 result.save_to_directory('doctests/test_unstratified_transe')
